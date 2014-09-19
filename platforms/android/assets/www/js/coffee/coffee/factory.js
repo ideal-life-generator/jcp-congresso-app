f43af75d2ff3dd4dd75935a38fc6b806f47ff6d6
nik <striletskyy@hotmail.com>
2014-09-19 18:02:11 +0300

    for android

--- platforms/android/assets/www/js/coffee/coffee/factory.js
+++ platforms/android/assets/www/js/coffee/coffee/factory.js
@@ -83,7 +83,7 @@
           var user;
           if (storage.getObject('user')) {
             user = storage.getObject('user');
-            if (user.version === "1.2.3") {
+            if (user.version === "1.2.1") {
               Auth.setCredentials(user.email, user.password);
               return user;
             } else {
@@ -106,7 +106,7 @@
             data = result.data;
             self.user.detail = data;
             data.password = password;
-            data.version = "1.2.3";
+            data.version = "1.2.1";
             storage.setObject('user', data);
             return defer.resolve(data);
           }, function(error) {
